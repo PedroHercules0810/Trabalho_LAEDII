0a7809dc88a8f54fcc963dcfb8b8c7509113a2af
mM-b <gtamsue@gmail.com>
2023-09-23 18:12:31 -0300

    change problem

--- listatel.h
+++ listatel.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #define SIZE 32
 typedef struct lista *agendaDeContatos[SIZE];
 
@@ -15,9 +14,3 @@ int removerContato(agendaDeContatos Hash, struct contato *c);
 void exportarContatos(agendaDeContatos Hash);
 
 void listarContatos(agendaDeContatos Hash);
-=======
-typedef struct contato Contato;
-typedef struct lista Lista;
-int dobra_binaria(char *chave);
-Contato * insere_contato(Lista * l,Contato * c, char * nome, char * tel, char * email);
->>>>>>> bffd035c24af7b59a18ec54c7b9ef16b214d8048
